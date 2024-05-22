b928317a925bf57523ffa81c0f270ec820613b02
Chernoles-Kirilo-KB-23 <144206780+Chernoles-Kirilo-KB-23@users.noreply.github.com>
2024-05-22 07:03:50 +0300

    Update LAB№8.2.1.cpp

--- lab08/tasks/LAB№8/LAB№8.2.1.cpp
+++ lab08/tasks/LAB№8/LAB№8.2.1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Чернолєс Кирило (С)" << std::endl;
+    std::cout << "Чернолєс Кирило (c) | Академ-група:КБ-23 | ЦНТУ" << std::endl;
 
     return 0;
 }
